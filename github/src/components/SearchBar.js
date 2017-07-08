1881cee92173678ac0756d8d7a3096d57b887799
changjianhong <cjh130@126.com>
2017-07-09 01:09:34 +0800

    searchBar

--- github/src/components/SearchBar.js
+++ github/src/components/SearchBar.js
@@ -1,42 +1,73 @@
 import React from 'react';
 import screen from '../common/screen';
-import { View, TextInput, StyleSheet, LayoutAnimation, NativeModules } from 'react-native';
+import { View, Image, TextInput, StyleSheet, LayoutAnimation, NativeModules } from 'react-native';
 import Button from 'apsl-react-native-button';
 
 const { UIManager } = NativeModules;
 
-UIManager.setLayoutAnimationEnabledExperimental &&
-  UIManager.setLayoutAnimationEnabledExperimental(true);
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+
+const searchBarAnimatedState = {
+  inputWidth: screen.width - 68,
+  cancelBtnTextColor: 'red',
+  iconMarginLeft: - screen.width / 2 + 22,
+  selectionColor: 'black'
+}
+
+const searchBarNoAnimatedState = {
+  inputWidth: screen.width - 40,
+  cancelBtnTextColor: 'transparent',
+  iconMarginLeft: -7,
+  selectionColor: 'transparent'
+}
 
 class SearchBar extends React.Component {
-
 	constructor(props) {
 		super(props);
-		this.state = {
-			btnDispaly: 'none',
-			inputWidth: screen.width - 40
-		}
+		this.state = {...searchBarNoAnimatedState}
 	}
 
 	_onFocus = () => {
-		LayoutAnimation.linear();
-		this.setState({
-			btnDispaly: 'flex',
-			inputWidth: screen.width - 80
-		})
+    let linear = LayoutAnimation.create(200, LayoutAnimation.Types.linear, LayoutAnimation.Properties.opacity);
+    LayoutAnimation.configureNext.bind(null, linear, () => {
+      this.setState({
+        selectionColor: searchBarAnimatedState.selectionColor
+      });
+    })();
+    this.setState({...searchBarAnimatedState, ...{selectionColor: searchBarNoAnimatedState.selectionColor}});
 	}
+
+  _onBlur = () => {
+    console.log('onBlur');
+    let linear = LayoutAnimation.create(200, LayoutAnimation.Types.linear, LayoutAnimation.Properties.opacity);
+    LayoutAnimation.configureNext.bind(null, linear, () => {
+      this.setState({
+        cancelBtnTextColor: searchBarNoAnimatedState.cancelBtnTextColor
+      });
+    })();
+    this.setState({...searchBarNoAnimatedState, ...{cancelBtnTextColor: searchBarAnimatedState.cancelBtnTextColor}});
+  }
+
+  _cancelBtnDidClicked = () => {
+    console.log('_cancelBtnDidClicked');
+    this._textInput.blur();
+  }
 
 	render() {
 		return (
 			<View style={styles.container}>
-
 				<TextInput
+          ref={textInput => (this._textInput = textInput)}
+          selectionColor = {this.state.selectionColor}
 					onFocus = {this._onFocus}
+          onBlur = {this._onBlur}
 					style={[styles.textInput, {width: this.state.inputWidth}]}
 					placeholder = '搜索'
 					placeholderTextColor = 'red'
 				/>
-				<Button style={[styles.button, {display: this.state.btnDispaly}]} textStyle={styles.buttonTextStyle}>取消</Button>
+				<Button style={[styles.button]} textStyle={[styles.buttonTextStyle, {color: this.state.cancelBtnTextColor}]} onPress={this._cancelBtnDidClicked}>取消</Button>
+        <Image source={require('../img/search.png')} style={[styles.icon, {marginLeft: this.state.iconMarginLeft}]}/>
+
 			</View>
 		)
 	}
@@ -55,20 +86,31 @@ const styles = StyleSheet.create({
 		height: 28,
 		marginLeft: 20,
 		marginTop: 8,
-		borderColor: 'red',
+		borderColor: 'transparent',
 		borderWidth: 1,
-		borderRadius: 8,
-		backgroundColor: 'grey'
+		borderRadius: 4,
+		backgroundColor: 'red',
+    paddingLeft: 18,
+    fontSize: 14
 	},
+  icon: {
+    position: 'absolute',
+    width: 14,
+    height: 14,
+    marginLeft: -7,
+    marginTop: -7,
+    top: '50%',
+    left: '50%'
+  },
 	button: {
 		marginTop: 8,
-		borderColor: 'red',
+    borderColor: 'transparent',
 		width: 44,
 		height: 28,
 		borderRadius: 0,
 	},
 	buttonTextStyle: {
-		fontSize: 12,
+		fontSize: 14,
 		color: '#777777',
 	}
 });
