69f2f0d604946789121f37d64753eb7b512c588b
changjianhong <cjh130@126.com>
2017-07-27 14:18:28 +0800

    。。。。。。

--- github/src/mine/MineScreen.js
+++ github/src/mine/MineScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
+import { TouchableOpacity, Text, Animated, StyleSheet } from 'react-native';
 import Actions from '../action/userActions';
 import { color } from '../common';
 import NavigationItem from '../components/NavigationItem';
 
 class MineScreen extends React.Component {
 	static navigationOptions = ({navigation}) => ({
-		headerTitle: '个人详情',
+		headerTitle: '我的',
 		headerStyle: {backgroundColor: color.pumpkin},
 		headerRight: (
       <NavigationItem
@@ -20,20 +20,51 @@ class MineScreen extends React.Component {
     )
 	})
 
+	constructor(props) {
+    super(props);
+    this.state = {
+      bounceValue: new Animated.Value(0),
+    };
+  }
+
 	render() {
 		const {login} = this.props.user;
 		return (
-			<Text>{login}</Text>
+			<TouchableOpacity style={styles.container} activeOpacity={1.0} onPress={this._startAnimation.bind(this)}>
+				<Animated.Text style={[styles.animatedView, {transform: [{scale: this.state.bounceValue}]}]}>
+					{login}
+				</Animated.Text>
+			</TouchableOpacity>
 		)
 	}
 
 	componentDidMount() {
-		this.props.getUser({name: 'changjianhong'}).then((json) => {
-			console.log(json);
+		this.props.getUser({name: 'changjianhong'}).then((json)=>{
+			this._startAnimation();
 		});
 	}
 
+	_startAnimation() {
+		this.state.bounceValue.setValue(1.5);
+    Animated.spring(
+      this.state.bounceValue,
+      {
+        toValue: 0.8,
+        friction: 1,
+      }
+    ).start();
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex:1
+	},
+	animatedView: {
+		fontSize: 32,
+		textAlign: 'center'
+	}
+})
 
 const mapStateToProps = (state) => {
 	let userData = state.user;
